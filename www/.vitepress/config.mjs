8ee125bffac07c03be895d73df9668e60126cbbb
Gabriel Nobrega <gnobrega.contato@gmail.com>
2025-02-21 18:14:35 -0300

    feat: change base in config

--- www/.vitepress/config.mjs
+++ www/.vitepress/config.mjs
@@ -22,30 +22,9 @@ export default defineConfig({
       gtag('config', 'G-7EF2NVE4ZK');`]
   ],
 
-  base: '/quickreceitas/',
+  base: '/',
+  // base: '/quickreceitas/',
 
-  // plugins: [
-  //   {
-  //     name: 'inject-scripts',
-  //     transformIndexHtml() {
-  //       return [
-  //         {
-  //           tag: 'script',
-  //           children: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "qd4y5g3j4f");`
-  //         },
-  //         {
-  //           tag: 'script',
-  //           src: `https://www.googletagmanager.com/gtag/js?id=G-7EF2NVE4ZK`,
-  //         },
-  //         {
-  //           tag: 'script',
-  //           children: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7EF2NVE4ZK');`
-  //         }
-  //       ]
-  //     }
-  //   }
-  // ],
-  
   themeConfig: {
     logo: {
       src: '/logo.png',
