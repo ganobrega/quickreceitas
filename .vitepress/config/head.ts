6e7dee0f3a68859d795798a4df2f838e796d0a29
Gabriel Nobrega <gnobrega.contato@gmail.com>
2025-03-11 14:19:08 -0300

    seo: performance + best pratices + csp policies

--- .vitepress/config/head.ts
+++ .vitepress/config/head.ts
@@ -1,48 +1,156 @@
-import type { HeadConfig } from 'vitepress'
+import type { HeadConfig } from "vitepress";
+
+const hosts = {
+  analytics: 'https://www.google-analytics.com',
+  tagManager: 'https://www.googletagmanager.com',
+  google: 'https://www.google.com',
+  googleBr: 'https://www.google.com.br'
+}
+
+const trustedHosts = Object.values(hosts).join(' ')
+
+const csp = [
+  // Base policies
+  "default-src 'self'",
+  
+  // Scripts
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'unsafe-hashes' " + trustedHosts,
+  
+  // Styles - permitir inline styles
+  "style-src 'self' 'unsafe-inline'",
+  
+  // Images - permitir SVGs inline e data URLs
+  "img-src 'self' data: blob: " + trustedHosts,
+  
+  // Fonts
+  "font-src 'self' data:",
+  
+  // Connect
+  "connect-src 'self' " + trustedHosts,
+  
+  // Frames
+  "frame-src 'self' " + trustedHosts,
+  
+  // Objects
+  "object-src 'self'",
+  
+  // Media
+  "media-src 'self'",
+  
+  // Worker
+  "worker-src 'self' blob:",
+  
+  // Manifest
+  "manifest-src 'self'"
+].join('; ')
 
 const head: HeadConfig[] = [
-  ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  ['link', { rel: 'manifest', href: '/manifest.json' }],
-  ['meta', { name: 'theme-color', content: '#06b6d4' }],
-  ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
-  ['meta', { name: 'author', content: 'Quick Receitas' }],
-  ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
+  // Links
+  ["link", { rel: "icon", type: "image/x-icon", href: "/images/favicon.ico" }],
+  ["link", { rel: "manifest", href: "/manifest.json" }],
+  ["link", { rel: "preconnect", href: hosts.tagManager }],
 
-  ['meta', { name: 'application-name', content: 'QuickReceitas' }],
-  ['meta', { name: 'apple-mobile-web-app-title', content: 'QuickReceitas' }],
-  ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-  ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
+  [
+    "link",
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/images/apple-touch-icon.png",
+    },
+  ],
+  [
+    "link",
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/images/favicon-32x32.png",
+    },
+  ],
+  [
+    "link",
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/images/favicon-16x16.png",
+    },
+  ],
 
-  ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/apple-touch-icon.png' }],
-  ['link', { rel: 'icon', type: "image/png", sizes: '32x32', href: '/images/favicon-32x32.png' }],
-  ['link', { rel: 'icon', type: "image/png", sizes: '16x16', href: '/images/favicon-16x16.png' }],
+  // Metatags
+  [
+    "meta",
+    {
+      "http-equiv": "Content-Security-Policy",
+      content: csp,
+    },
+  ],
 
-  ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-  ['meta', { name: 'twitter:site', content: '@quickreceitas' }],
-  ['meta', { name: 'twitter:creator', content: '@quickreceitas' }],
-  ['meta', { name: 'twitter:image:width', content: '800' }],
-  ['meta', { name: 'twitter:image:height', content: '420' }],
+  ["meta", { name: "theme-color", content: "#06b6d4" }],
 
-  ['meta', { property: 'og:site_name', content: 'QuickReceitas' }],
-  ['meta', { property: 'og:description', content: 'Receitas rápidas e práticas para seu dia a dia' }],
-  ['meta', { property: 'og:url', content: `${process.env.VITE_BASE_URL}` }],
-  ['meta', { property: 'og:locale', content: 'pt_BR' }],
-  ['meta', { property: 'og:type', content: 'website' }],
-  ['meta', { property: 'og:image:width', content: '800' }],
-  ['meta', { property: 'og:image:height', content: '420' }],
-  ['meta', { property: 'og:image:type', content: 'image/png' }],
-  ['meta', { property: 'og:image:alt', content: 'QuickReceitas - Receitas práticas' }],
-  ['meta', { property: 'og:image', content: `${process.env.VITE_BASE_URL}/images/og-image.png?v=1` }],
-  ['script', { 
-    src: 'https://www.googletagmanager.com/gtag/js?id=G-7EF2NVE4ZK',
-    async: 'true',
-    defer: 'true'
-  }],
+  [
+    "meta",
+    { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+  ],
+  ["meta", { name: "author", content: "Quick Receitas" }],
+  ["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
+  ["meta", { name: "application-name", content: "QuickReceitas" }],
+  ["meta", { name: "apple-mobile-web-app-title", content: "QuickReceitas" }],
+  ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+  [
+    "meta",
+    { name: "apple-mobile-web-app-status-bar-style", content: "default" },
+  ],
 
-  ['script', {
-    async: 'true',
-    defer: 'true'
-  }, `
+  ["meta", { name: "twitter:card", content: "summary_large_image" }],
+  ["meta", { name: "twitter:site", content: "@quickreceitas" }],
+  ["meta", { name: "twitter:creator", content: "@quickreceitas" }],
+  ["meta", { name: "twitter:image:width", content: "800" }],
+  ["meta", { name: "twitter:image:height", content: "420" }],
+
+  ["meta", { property: "og:site_name", content: "QuickReceitas" }],
+  [
+    "meta",
+    {
+      property: "og:description",
+      content: "Receitas rápidas e práticas para seu dia a dia",
+    },
+  ],
+  ["meta", { property: "og:url", content: `${process.env.VITE_BASE_URL}` }],
+  ["meta", { property: "og:locale", content: "pt_BR" }],
+  ["meta", { property: "og:type", content: "website" }],
+  ["meta", { property: "og:image:width", content: "800" }],
+  ["meta", { property: "og:image:height", content: "420" }],
+  ["meta", { property: "og:image:type", content: "image/png" }],
+  [
+    "meta",
+    { property: "og:image:alt", content: "QuickReceitas - Receitas práticas" },
+  ],
+  [
+    "meta",
+    {
+      property: "og:image",
+      content: `${process.env.VITE_BASE_URL}/images/og-image.png?v=1`,
+    },
+  ],
+
+  // Scripts
+  [
+    "script",
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=G-7EF2NVE4ZK",
+      async: "true",
+      defer: "true",
+    },
+  ],
+
+  [
+    "script",
+    {
+      async: "true",
+      defer: "true",
+    },
+    `
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
@@ -53,7 +161,8 @@ const head: HeadConfig[] = [
       'anonymize_ip': true,
       'cookie_domain': 'none'
     });
-  `]
-]
+  `,
+  ],
+];
 
-export default head
+export default head;
